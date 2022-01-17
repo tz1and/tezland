52693700b60dd9bea137313365eb5777794f81a9
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-01-17 20:00:16 +0100

    test more FA2 configs

--- tests/FA2_tests.py
+++ tests/FA2_tests.py
@@ -2,23 +2,18 @@ import smartpy as sp
 
 fa2_contract = sp.io.import_script_from_url("file:contracts/FA2.py")
 
+# test our specific config
 fa2_contract.add_test(fa2_contract.items_config())
 fa2_contract.add_test(fa2_contract.places_config())
-#if not fa2_contract.global_parameter("only_environment_test", False):
-#    fa2_contract.add_test(fa2_contract.FA2_config(debug_mode = True), is_default = not sp.in_browser)
-#    fa2_contract.add_test(fa2_contract.FA2_config(single_asset = True), is_default = not sp.in_browser)
-#    fa2_contract.add_test(fa2_contract.FA2_config(non_fungible = True, add_mutez_transfer = True),
-#             is_default = not sp.in_browser)
-#    fa2_contract.add_test(fa2_contract.FA2_config(readable = False), is_default = not sp.in_browser)
-#    fa2_contract.add_test(fa2_contract.FA2_config(force_layouts = False),
-#             is_default = not sp.in_browser)
-#    fa2_contract.add_test(fa2_contract.FA2_config(debug_mode = True, support_operator = False),
-#             is_default = not sp.in_browser)
-#    fa2_contract.add_test(fa2_contract.FA2_config(assume_consecutive_token_ids = False)
-#             , is_default = not sp.in_browser)
-#    fa2_contract.add_test(fa2_contract.FA2_config(store_total_supply = True)
-#             , is_default = not sp.in_browser)
-#    fa2_contract.add_test(fa2_contract.FA2_config(add_mutez_transfer = True)
-#             , is_default = not sp.in_browser)
-#    fa2_contract.add_test(fa2_contract.FA2_config(lazy_entry_points = True)
-#             , is_default = not sp.in_browser)
+
+# test all config
+fa2_contract.add_test(fa2_contract.FA2_config(debug_mode = True))
+fa2_contract.add_test(fa2_contract.FA2_config(single_asset = True))
+fa2_contract.add_test(fa2_contract.FA2_config(non_fungible = True, add_mutez_transfer = True))
+fa2_contract.add_test(fa2_contract.FA2_config(readable = False))
+fa2_contract.add_test(fa2_contract.FA2_config(force_layouts = False))
+fa2_contract.add_test(fa2_contract.FA2_config(debug_mode = True, support_operator = False))
+fa2_contract.add_test(fa2_contract.FA2_config(assume_consecutive_token_ids = False))
+fa2_contract.add_test(fa2_contract.FA2_config(store_total_supply = True))
+fa2_contract.add_test(fa2_contract.FA2_config(add_mutez_transfer = True))
+fa2_contract.add_test(fa2_contract.FA2_config(lazy_entry_points = True))
