6018d307b2ec45bdb7758e57499f6c253ea25358
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-02-13 18:45:45 +0100

    fix: create deployments dir after deleting

--- src/commands/DeployBase.ts
+++ src/commands/DeployBase.ts
@@ -120,11 +120,17 @@ export default class DeployBase {
         assert(this.networkConfig.accounts.deployer, `deployer account not set for '${this.network}'`)
 
         this.deploymentsDir = `./deployments/${this.network}`;
+        
         // if sandbox, delete deployments dir
-        if(this.isSandboxNet)
-            if (fs.existsSync(this.deploymentsDir)) fs.rmdirSync(this.deploymentsDir, { recursive: true });
-        else
+        if(this.isSandboxNet) {
+            if (fs.existsSync(this.deploymentsDir)) {
+                fs.rmdirSync(this.deploymentsDir, { recursive: true });
+                fs.mkdirSync(this.deploymentsDir, { recursive: true });
+            }
+        }
+        else {
             if (!fs.existsSync(this.deploymentsDir)) fs.mkdirSync(this.deploymentsDir, { recursive: true });
+        }
 
         this.deploymentsRegPath =`${this.deploymentsDir}/project.deployments.json`;
         if (fs.existsSync(this.deploymentsRegPath))
