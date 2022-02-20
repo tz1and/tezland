f51d822cb9fccc9a42a7d799fa998a7f15ef7ca7
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-02-20 02:23:37 +0100

    add Upgradeable mixin

---
+++ contracts/Upgradeable.py
@@ -0,0 +1,21 @@
+import smartpy as sp
+
+admin_contract = sp.io.import_script_from_url("file:contracts/Administrable.py")
+
+class Upgradeable(admin_contract.Administrable):
+    def __init__(self, administrator, entrypoints: list[str]):
+        self.upgradeable_entrypoints = entrypoints
+        admin_contract.Administrable.__init__(self, administrator = administrator)
+
+    @sp.entry_point
+    def update_ep(self, params):
+        # Build a variant from upgradeable_entrypoints.
+        sp.set_type(params.ep_name, sp.TVariant(
+            **{entrypoint: sp.TUnit for entrypoint in self.upgradeable_entrypoints}))
+        self.onlyAdministrator()
+
+        # Build a matcher for upgradeable_entrypoints
+        with params.ep_name.match_cases() as arg:
+            for entrypoint in self.upgradeable_entrypoints:
+                with arg.match(entrypoint):
+                    sp.set_entry_point(entrypoint, params.new_code)
---
+++ tests/Upgradeable_tests.py
@@ -0,0 +1,59 @@
+import smartpy as sp
+
+upgradeable = sp.io.import_script_from_url("file:contracts/Upgradeable.py")
+
+class UpgradeableTest(upgradeable.Upgradeable):
+    def __init__(self, administrator):
+        self.init_storage(
+            counter = sp.int(0)
+        )
+        upgradeable.Upgradeable.__init__(self, administrator = administrator,
+            entrypoints = ['test_entry', 'another_entry'])
+
+    @sp.entry_point(lazify = True)
+    def test_entry(self, params):
+        sp.set_type(params.val, sp.TInt)
+        self.data.counter += params.val
+
+    @sp.entry_point(lazify = True)
+    def another_entry(self, params):
+        sp.set_type(params.val, sp.TInt)
+        self.data.counter *= params.val
+
+
+def test_entry_uodate(self, params):
+    sp.set_type(params.val, sp.TInt)
+    self.data.counter -= params.val
+
+def another_entry_update(self, params):
+    sp.set_type(params.val, sp.TInt)
+    self.data.counter = params.val
+
+@sp.add_test(name = "UpgradeableTest_tests", profile = True)
+def test():
+    admin = sp.test_account("Administrator")
+    alice = sp.test_account("Alice")
+    bob   = sp.test_account("Robert")
+    scenario = sp.test_scenario()
+
+    # create upgrade contract
+    upgrade = UpgradeableTest(admin.address) #admin.address, 
+    scenario += upgrade
+
+    # test entry
+    upgrade.test_entry(val = sp.int(2)).run(sender = alice)
+    scenario.verify(upgrade.data.counter == sp.int(2))
+
+    upgrade.another_entry(val = sp.int(2)).run(sender = alice)
+    scenario.verify(upgrade.data.counter == sp.int(4))
+
+    # update
+    upgrade.update_ep(ep_name = sp.variant('test_entry', sp.unit), new_code = sp.utils.wrap_entry_point("test_entry", test_entry_uodate)).run(sender = admin)
+    upgrade.update_ep(ep_name = sp.variant('another_entry', sp.unit), new_code = sp.utils.wrap_entry_point("another_entry", another_entry_update)).run(sender = admin)
+
+    # test updated entrypoints
+    upgrade.test_entry(val = sp.int(5)).run(sender = alice)
+    scenario.verify(upgrade.data.counter == sp.int(-1))
+
+    upgrade.another_entry(val = sp.int(1337)).run(sender = alice)
+    scenario.verify(upgrade.data.counter == sp.int(1337))
