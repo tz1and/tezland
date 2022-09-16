dcb9ddadd3aaa62baee80aee07c86b375bddebff
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-09-16 18:21:08 +0200

    AllowedPlaceTokens: limits, no bigmap

--- contracts/AllowedPlaceTokens.py
+++ contracts/AllowedPlaceTokens.py
@@ -4,15 +4,20 @@ admin_mixin = sp.io.import_script_from_url("file:contracts/Administrable.py")
 utils = sp.io.import_script_from_url("file:contracts/Utils.py")
 FA2 = sp.io.import_script_from_url("file:contracts/FA2.py")
 
+
+allowedPlaceLimitsType = sp.TRecord(
+    chunk_limit = sp.TNat,
+    chunk_item_limit = sp.TNat
+).layout(("chunk_limit", "chunk_item_limit"))
+
 #
 # Lazy map of allowed place tokens.
 class Allowed_place_token_map:
     def make(self, default_allowed):
-        # TODO: does this need to be a bigmap???
-        return sp.big_map(l = default_allowed, tkey = sp.TAddress, tvalue = sp.TUnit)
+        return sp.map(l = default_allowed, tkey = sp.TAddress, tvalue = allowedPlaceLimitsType)
 
-    def add(self, map, fa2):
-        map[fa2] = sp.unit
+    def add(self, map, fa2, place_limits):
+        map[fa2] = place_limits
 
     def remove(self, map, fa2):
         del map[fa2]
@@ -20,12 +25,20 @@ class Allowed_place_token_map:
     def is_allowed(self, map, fa2):
         return map.contains(fa2)
 
+    def get_limits(self, map, fa2):
+        return map.get(fa2, message = "PLACE_TOKEN_NOT_ALLOWED")
+
 class Allowed_place_token_param:
     def get_add_type(self):
-        return sp.TAddress
+        return sp.TRecord(
+            fa2 = sp.TAddress,
+            place_limits = allowedPlaceLimitsType
+        ).layout(("fa2", "place_limits"))
 
-    def make_add(self, fa2):
-        return sp.set_type_expr(fa2, self.get_add_type())
+    def make_add(self, fa2, place_limits):
+        r = sp.record(fa2 = fa2,
+            place_limits = place_limits)
+        return sp.set_type_expr(r, self.get_add_type())
 
     def get_remove_type(self):
         return sp.TAddress
@@ -62,8 +75,14 @@ class AllowedPlaceTokens(admin_mixin.Administrable):
         return sp.compute(self.allowed_place_tokens_map.is_allowed(self.data.allowed_place_tokens, fa2))
 
 
+    def getAllowedPlaceToken(self, fa2):
+        """Returns allowed place limits or fails"""
+        fa2 = sp.set_type_expr(fa2, sp.TAddress)
+        return sp.compute(self.allowed_place_tokens_map.get_limits(self.data.allowed_place_tokens, fa2))
+
+
     @sp.entry_point
-    def set_place_token_allowed(self, params):
+    def set_allowed_place_token(self, params):
         """Call to add/remove place token contracts from
         token contracts allowed in the world."""
         sp.set_type(params, sp.TList(sp.TVariant(
@@ -76,14 +95,20 @@ class AllowedPlaceTokens(admin_mixin.Administrable):
         with sp.for_("update", params) as update:
             with update.match_cases() as arg:
                 with arg.match("add_allowed_place_token") as upd:
-                    self.allowed_place_tokens_map.add(self.data.allowed_place_tokens, upd)
+                    self.allowed_place_tokens_map.add(self.data.allowed_place_tokens, upd.fa2, upd.place_limits)
                 with arg.match("remove_allowed_place_token") as upd:
                     self.allowed_place_tokens_map.remove(self.data.allowed_place_tokens, upd)
 
 
     @sp.onchain_view(pure=True)
-    def is_place_token_allowed(self, fa2):
+    def is_allowed_place_token(self, fa2):
         """Returns True if an fa2 place token is allowed."""
         sp.set_type(fa2, sp.TAddress)
         sp.result(self.allowed_place_tokens_map.is_allowed(self.data.allowed_place_tokens, fa2))
+
+    @sp.onchain_view(pure=True)
+    def get_allowed_place_token_limits(self, fa2):
+        """Returns allowed place token limits."""
+        sp.set_type(fa2, sp.TAddress)
+        sp.result(self.allowed_place_tokens_map.get_limits(self.data.allowed_place_tokens, fa2))
 
--- tests/AllowedPlaceTokens_tests.py
+++ tests/AllowedPlaceTokens_tests.py
@@ -17,6 +17,11 @@ class AllowedPlaceTokensTest(allowed_place_tokens.AllowedPlaceTokens, sp.Contrac
         sp.set_type(params, sp.TRecord(fa2 = sp.TAddress, expected = sp.TBool))
         sp.verify(self.isAllowedPlaceToken(params.fa2) == params.expected, "unexpected result")
 
+    @sp.entry_point
+    def testGetAllowedPlaceToken(self, params):
+        sp.set_type(params, sp.TRecord(fa2 = sp.TAddress, expected = allowed_place_tokens.allowedPlaceLimitsType))
+        sp.verify(self.getAllowedPlaceToken(params.fa2) == params.expected, "unexpected result")
+
 
 @sp.add_test(name = "AllowedPlaceTokens_tests", profile = True)
 def test():
@@ -47,35 +52,47 @@ def test():
     scenario += other_token
 
     # test set allowed
-    scenario.h3("set_place_token_allowed")
-    add_allowed = sp.list([sp.variant("add_allowed_place_token", other_token.address)])
+    scenario.h3("set_allowed_place_token")
+    test_place_limits = sp.record(chunk_limit = 1, chunk_item_limit = 64)
+    add_allowed = sp.list([sp.variant("add_allowed_place_token", sp.record(fa2 = other_token.address, place_limits = test_place_limits))])
     remove_allowed = sp.list([sp.variant("remove_allowed_place_token", other_token.address)])
 
     # no permission
-    allowedPlaceTokens.set_place_token_allowed(add_allowed).run(sender = bob, valid = False, exception = "ONLY_ADMIN")
+    allowedPlaceTokens.set_allowed_place_token(add_allowed).run(sender = bob, valid = False, exception = "ONLY_ADMIN")
     scenario.verify(allowedPlaceTokens.data.allowed_place_tokens.contains(other_token.address) == False)
 
     # add
-    allowedPlaceTokens.set_place_token_allowed(add_allowed).run(sender = admin)
+    allowedPlaceTokens.set_allowed_place_token(add_allowed).run(sender = admin)
     scenario.verify(allowedPlaceTokens.data.allowed_place_tokens.contains(other_token.address) == True)
+    scenario.verify(allowedPlaceTokens.data.allowed_place_tokens[other_token.address] == test_place_limits)
 
     # remove
-    allowedPlaceTokens.set_place_token_allowed(remove_allowed).run(sender = admin)
+    allowedPlaceTokens.set_allowed_place_token(remove_allowed).run(sender = admin)
     scenario.verify(allowedPlaceTokens.data.allowed_place_tokens.contains(other_token.address) == False)
     scenario.verify(sp.is_failing(allowedPlaceTokens.data.allowed_place_tokens[other_token.address]))
 
-    # test get
-    scenario.h3("is_place_token_allowed view")
-    scenario.verify(allowedPlaceTokens.is_place_token_allowed(other_token.address) == False)
-    allowedPlaceTokens.set_place_token_allowed(add_allowed).run(sender = admin)
-    scenario.verify(allowedPlaceTokens.is_place_token_allowed(other_token.address) == True)
+    # test views
+    scenario.h3("is_allowed_place_token view")
+    scenario.verify(allowedPlaceTokens.is_allowed_place_token(other_token.address) == False)
+    allowedPlaceTokens.set_allowed_place_token(add_allowed).run(sender = admin)
+    scenario.verify(allowedPlaceTokens.is_allowed_place_token(other_token.address) == True)
 
-    scenario.h3("testOnlyAllowedPlaceTokens")
-    allowedPlaceTokens.testOnlyAllowedPlaceTokens(other_token.address).run(sender = admin)
-    allowedPlaceTokens.set_place_token_allowed(remove_allowed).run(sender = admin)
-    allowedPlaceTokens.testOnlyAllowedPlaceTokens(other_token.address).run(sender = admin, valid = False, exception = "PLACE_TOKEN_NOT_ALLOWED")
+    scenario.h3("get_allowed_place_token_limits view")
+    scenario.verify(allowedPlaceTokens.get_allowed_place_token_limits(other_token.address) == test_place_limits)
+    allowedPlaceTokens.set_allowed_place_token(remove_allowed).run(sender = admin)
+    scenario.verify(sp.is_failing(allowedPlaceTokens.get_allowed_place_token_limits(other_token.address)))
 
     scenario.h3("testIsAllowedPlaceToken")
     allowedPlaceTokens.testIsAllowedPlaceToken(sp.record(fa2 = other_token.address, expected = False)).run(sender = admin)
-    allowedPlaceTokens.set_place_token_allowed(add_allowed).run(sender = admin)
+    allowedPlaceTokens.set_allowed_place_token(add_allowed).run(sender = admin)
     allowedPlaceTokens.testIsAllowedPlaceToken(sp.record(fa2 = other_token.address, expected = True)).run(sender = admin)
+
+    scenario.h3("testOnlyAllowedPlaceTokens")
+    allowedPlaceTokens.testOnlyAllowedPlaceTokens(other_token.address).run(sender = admin)
+    allowedPlaceTokens.set_allowed_place_token(remove_allowed).run(sender = admin)
+    allowedPlaceTokens.testOnlyAllowedPlaceTokens(other_token.address).run(sender = admin, valid = False, exception = "PLACE_TOKEN_NOT_ALLOWED")
+
+    scenario.h3("testGetAllowedPlaceTokens")
+    allowedPlaceTokens.testGetAllowedPlaceToken(sp.record(fa2 = other_token.address, expected = test_place_limits)).run(sender = admin, valid = False, exception = "PLACE_TOKEN_NOT_ALLOWED")
+    allowedPlaceTokens.set_allowed_place_token(add_allowed).run(sender = admin)
+    allowedPlaceTokens.testGetAllowedPlaceToken(sp.record(fa2 = other_token.address, expected = test_place_limits)).run(sender = admin)
