4c484f5921b15f9a069f1a7018af686c1df986b8
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-10-21 02:48:39 +0200

    world v1.1 migration stuff

--- contracts/upgrades/TL_World_v1_1.py
+++ contracts/upgrades/TL_World_v1_1.py
@@ -2,7 +2,11 @@ import smartpy as sp
 
 world_contract = sp.io.import_script_from_url("file:contracts/TL_World.py")
 worldv2_contract = sp.io.import_script_from_url("file:contracts/TL_World_v2.py")
+FA2 = sp.io.import_script_from_url("file:contracts/FA2.py")
 
+
+# TODO: instead of pulling tokens in v2 migrate, we could also transfer the from v1 to v2.
+#       I suppose it's a little risky, but if done carefully, it should save some fees.
 
 class TL_World_v1_1(world_contract.TL_World):
     def __init__(self, administrator, items_contract, places_contract, dao_contract, metadata,
@@ -17,17 +21,37 @@ class TL_World_v1_1(world_contract.TL_World):
         sp.set_type(migration_map, worldv2_contract.migrationItemMapType)
         sp.set_type(place_props, worldv2_contract.placePropsType)
         sp.set_type(lot_id, sp.TNat)
+
+        # Call v2 migration ep.
         migration_handle = sp.contract(
             worldv2_contract.migrationType,
             migration_to_contract,
             entry_point='migration').open_some()
         sp.transfer(sp.record(
             place_key = sp.record(place_contract = self.data.places_contract, lot_id = lot_id),
-            # For migration from v1 we basically need the same data as a chunk but with a list as the leaf.
             migrate_item_map = migration_map,
             migrate_place_props = place_props,
             extension = sp.none
         ), sp.mutez(0), migration_handle)
+
+    def update_adhoc_operators(self, token_contract, token_id_set):
+        sp.set_type(token_contract, sp.TAddress)
+        sp.set_type(token_id_set, sp.TSet(sp.TNat))
+
+        # Build operator permissions list.
+        operator_permissions = sp.local("operator_permissions", [], sp.TList(FA2.t_adhoc_operator_permission))
+        with sp.for_("id", token_id_set.elements()) as id:
+            operator_permissions.value.push(sp.record(
+                operator=sp.self_address,
+                token_id=id))
+
+        # Call token contract to add operators.
+        token_handle = sp.contract(
+            FA2.t_adhoc_operator_params,
+            token_contract,
+            entry_point='update_adhoc_operators').open_some()
+        sp.transfer(sp.variant("add_adhoc_operators", operator_permissions.value),
+            sp.mutez(0), token_handle)
 
     @sp.entry_point(lazify = True)
     def set_item_data(self, params):
@@ -42,45 +66,52 @@ class TL_World_v1_1(world_contract.TL_World):
 
         self.onlyAdministrator()
 
-        migration_map = sp.local("migration_map", {}, worldv2_contract.migrationItemMapType)
+        this_place_opt = self.data.places.get_opt(params.lot_id)
 
-        this_place = self.place_store_map.get(self.data.places, params.lot_id)
+        # Do nothing for empty places.
+        with sp.if_(this_place_opt.is_some()):
+            this_place = this_place_opt.open_some()
 
-        # TODO: do nothing or fail for empty places.
-        # depends if I want to detect them advance or call any place anyway and waste a little gas...
+            migration_map = sp.local("migration_map", {}, worldv2_contract.migrationItemMapType)
+            token_id_set = sp.local("token_id_set", sp.set([]), sp.TSet(sp.TNat))
 
-        # Fill migration map with items from storage
-        with sp.for_("issuer", this_place.stored_items.keys()) as issuer:
-            # Get item store - must exist.
-            item_store = self.item_store_map.get(this_place.stored_items, issuer)
+            # Fill migration map with items from storage
+            with sp.for_("issuer", this_place.stored_items.keys()) as issuer:
+                # Get item store - must exist.
+                item_store = self.item_store_map.get(this_place.stored_items, issuer)
 
-            migration_map.value[issuer] = sp.map({})
-            migration_map.value[issuer][self.data.items_contract] = sp.list([])
-            current_list = migration_map.value[issuer][self.data.items_contract]
+                migration_map.value[issuer] = sp.map({})
+                migration_map.value[issuer][self.data.items_contract] = sp.list([])
+                current_list = migration_map.value[issuer][self.data.items_contract]
 
-            with sp.for_("update", item_store.values()) as item_variant:
-                with item_variant.match_cases() as arg:
-                    with arg.match("item") as item:
-                        # TODO: add adhoc operators
-                        current_list.push(sp.variant("item", sp.record(
-                            token_amount = item.item_amount,
-                            token_id = item.token_id,
-                            mutez_per_token = item.mutez_per_item,
-                            item_data = item.item_data)))
-                    with arg.match("other"):
-                        sp.failwith("OTHER_TYPE_ITEMS_NOT_SUPPORTED")
-                    with arg.match("ext") as ext:
-                        current_list.push(sp.variant("ext", ext))
+                with sp.for_("update", item_store.values()) as item_variant:
+                    with item_variant.match_cases() as arg:
+                        with arg.match("item") as item:
+                            # Add to token_id_set for adhoc operators.
+                            token_id_set.value.add(item.token_id)
+                            # Add item to migration list.
+                            current_list.push(sp.variant("item", sp.record(
+                                token_amount = item.item_amount,
+                                token_id = item.token_id,
+                                mutez_per_token = item.mutez_per_item,
+                                item_data = item.item_data)))
+                        with arg.match("other"):
+                            sp.failwith("OTHER_TYPE_ITEMS_NOT_SUPPORTED")
+                        with arg.match("ext") as ext:
+                            current_list.push(sp.variant("ext", ext))
 
-        # TODO: update adhoc operators.
+            # Update adhoc operators.
+            with sp.if_(sp.len(token_id_set.value) > 0):
+                self.update_adhoc_operators(self.data.items_contract, token_id_set.value)
 
-        # Send migration to world v2.
-        # TODO: somehow get the world v2 contract in here. maybe a constructor arg?
-        # TODO: only send if there actually are items to transfer, I guess.
-        self.send_migration(self.data.places_contract, migration_map.value, self.data.places[params.lot_id].place_props, params.lot_id)
+            # Send migration to world v2.
+            # TODO: somehow get the world v2 contract in here. maybe a constructor arg?
+            migrate_to_contract = self.data.places_contract
+            # NOTE: still have to send migration even if map is empty, for the props.
+            self.send_migration(migrate_to_contract, migration_map.value, self.data.places[params.lot_id].place_props, params.lot_id)
 
-        # Finally, delete the place from storage.
-        del self.data.places[params.lot_id]
+            # Finally, delete the place from storage.
+            del self.data.places[params.lot_id]
 
     @sp.entry_point(lazify = True)
     def get_item(self, params):
