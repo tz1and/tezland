3f6c6fcb258bc51c768f3187faca3bb0a90ce88b
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-11-10 20:28:58 +0100

    ipfs upload changes

--- src/storage.ts
+++ src/storage.ts
@@ -1,16 +1,18 @@
 // This file is more or less straight from the backend
 // Should probably rather include it or something...
 
-import { Blob, NFTStorage, Service, Token } from 'nft.storage'
+import { CarReader, NFTStorage, Token } from 'nft.storage'
+import { TreewalkCarJoiner } from 'carbites/treewalk'
 import * as ipfs from 'ipfs-http-client';
 import pkg from 'ipfs-utils/src/http';
 const { TimeoutError } = pkg;
 import { performance } from 'perf_hooks';
 import config from './user.config';
+import assert from 'assert';
 //import { Blockstore } from 'nft.storage/dist/src/platform';
 
 
-const ipfs_client = ipfs.create({ url: config.ipfs.localNodeUrl, timeout: 10000 });
+const ipfs_client = ipfs.create({ url: config.ipfs.localNodeUrl, timeout: 30000 });
 
 
 const validateTZip12 = ({ name, description, decimals }: { name: string, description: string, decimals: number}) => {
@@ -30,53 +32,61 @@ const validateTZip12 = ({ name, description, decimals }: { name: string, descrip
     }
 }
 
-// extend NFTStorage to allow us to supply our own validation while keeping
-// the simplicity of the store() function.
-class NFTStorageTZip extends NFTStorage {
-    static override async encodeNFT(input: any) {
-        validateTZip12(input)
+const noValidation = ({}) => {}
+
+class IPFSUpload {
+    private static encodeNFT(input: any) {
         return Token.Token.encode(input)
     }
 
-    static override async store(service: Service, metadata: any, options?: any) {
-        const { token, car } = await NFTStorageTZip.encodeNFT(metadata)
-        await NFTStorageTZip.storeCar(service, car, options)
-        return token
+    private static async storeNFTStorage(client: NFTStorage, car: CarReader, options?: any) {
+        const start_time = performance.now();
+        await NFTStorage.storeCar(client, car, options)
+        console.log(`upload to NFTStorage took ${(performance.now() - start_time).toFixed(2)} ms`);
     }
 
-    override store(token: any, options?: any) {
-        return NFTStorageTZip.store(this, token, options)
-    }
-}
-
-// Upload to NFTStorage without validation. For contract metadata.
-class NFTStorageNoValidation extends NFTStorage {
-    static override async encodeNFT(input: any) {
-        return Token.Token.encode(input)
-    }
-
-    static override async store(service: Service, metadata: any, options?: any) {
-        const { token, car } = await NFTStorageNoValidation.encodeNFT(metadata)
-        await NFTStorageNoValidation.storeCar(service, car, options)
-        return token
+    private static async storeLocalNode(local_client: ipfs.IPFSHTTPClient, token: Token.Token<any>, car: CarReader) {
+        const start_time = performance.now();
+        // Import car into local node.
+        // TODO: figure out if I need to pin roots.
+        const joiner = new TreewalkCarJoiner([car]);
+        for await (const local_upload of local_client.dag.import(joiner.car(), { pinRoots: false })) {
+            assert(local_upload.root.cid.toString() == token.ipnft)
+            console.dir(local_upload, {depth: 100});
+            console.dir(token, {depth: 100});
+        }
+        console.log(`upload to local ipfs took ${(performance.now() - start_time).toFixed(2)} ms`);
     }
 
-    override store(token: any, options?: any) {
-        return NFTStorageNoValidation.store(this, token, options)
+    static async store(metadata: any, validator: (metadata: any) => void, local_client: ipfs.IPFSHTTPClient, client?: NFTStorage, options?: any) {
+        // Validate token.
+        validator(metadata);
+
+        // Encode nft
+        const { token, car } = await IPFSUpload.encodeNFT(metadata);
+
+        // Upload to local node (for immediate availability).
+        await IPFSUpload.storeLocalNode(local_client, token, car);
+
+        // upload same car to NFT storage.
+        if (client) await IPFSUpload.storeNFTStorage(client, car, options);
+
+        // return token hash.
+        return token;
     }
 }
 
 //
 // Get root file from dag using ls.
 //
-async function get_root_file_from_dir(cid: string): Promise<string> {
+async function get_root_file_from_dir(local_client: ipfs.IPFSHTTPClient, cid: string): Promise<string> {
     console.log("get_root_file_from_dir: ", cid)
     try {
         const max_num_retries = 10;
         let num_retries = 0;
         while(num_retries < max_num_retries) {
             try {
-                for await (const entry of ipfs_client.ls(cid)) {
+                for await (const entry of local_client.ls(cid)) {
                     //console.log(entry)
                     if (entry.type === 'file') {
                         return entry.cid.toString();
@@ -101,14 +111,14 @@ async function get_root_file_from_dir(cid: string): Promise<string> {
 //
 // Resolve path to CID using dag.resolve.
 //
-async function resolve_path_to_cid(path: string): Promise<string> {
+async function resolve_path_to_cid(local_client: ipfs.IPFSHTTPClient, path: string): Promise<string> {
     console.log("resolve_path_to_cid: ", path)
     try {
         const max_num_retries = 10;
         let num_retries = 0;
         while(num_retries < max_num_retries) {
             try {
-                const resolve_result = await ipfs_client.dag.resolve(path);
+                const resolve_result = await local_client.dag.resolve(path);
 
                 // If there's a remainder path, something probably went wrong.
                 if (resolve_result.remainderPath && resolve_result.remainderPath.length > 0)
@@ -139,59 +149,15 @@ type ResultType = {
     cid: string,
 }
 
-type handlerFunction = (data: any, is_contract: boolean, verbose: boolean) => Promise<ResultType>;
+export const uploadToIpfs = async (data: any, is_contract: boolean, localIpfs: boolean, verbose: boolean = false): Promise<ResultType> => {
+    const client = localIpfs ? undefined : new NFTStorage({ token: config.ipfs.nftStorageApiKey });
+    const validator = is_contract ? noValidation : validateTZip12;
 
-const uploadToNFTStorage: handlerFunction = async (data: any, is_contract: boolean, verbose: boolean): Promise<ResultType> => {
-    const start_time = performance.now();
-    const client = is_contract ? new NFTStorageNoValidation({ token: config.ipfs.nftStorageApiKey }) : new NFTStorageTZip({ token: config.ipfs.nftStorageApiKey })
+    // Upload to local and maybe NFT storage.
+    const metadata = await IPFSUpload.store(data, validator, ipfs_client, client);
 
-    const metadata = await client.store(data);
-    const file_cid = await resolve_path_to_cid(`${metadata.ipnft}/metadata.json`);
-    if (verbose) console.log("uploadToNFTStorage took " + (performance.now() - start_time).toFixed(2) + "ms");
+    // Get cid of metadata file.
+    const file_cid = await resolve_path_to_cid(ipfs_client, `${metadata.ipnft}/metadata.json`);
 
     return { metdata_uri: `ipfs://${file_cid}`, cid: file_cid };
-}
-
-const uploadToLocal: handlerFunction = async (data: any, is_contract: boolean, verbose: boolean): Promise<ResultType> => {
-    const start_time = performance.now()
-
-    // first we upload every blob in the object
-    const traverse = async (jsonObj: any) => {
-        if( jsonObj !== null && typeof jsonObj == "object" ) {
-            // key is either an array index or object key
-            for (const [key, value] of Object.entries(jsonObj)) {
-                // if it's a File, upload it.
-                if(value instanceof Blob) {
-                    const file: Blob = value as Blob;
-        
-                    // upload to ips
-                    const result = await ipfs_client.add(file);
-                    const path = `ipfs://${result.cid.toV0().toString()}`;
-        
-                    // and set the object to be the path
-                    jsonObj[key] = path;
-                }
-                else await traverse(value);
-            };
-        }
-        else {
-            // jsonObj is a number or string
-        }
-    }
-    await traverse(data);
-
-    // now upload the metadata:
-    const metadata = JSON.stringify(data);
-
-    const result = await ipfs_client.add(metadata);
-    if (verbose) console.log("uploadToLocal took " + (performance.now() - start_time).toFixed(2) + "ms");
-
-    const CIDstr = result.cid.toV0().toString();
-
-    return { metdata_uri: `ipfs://${CIDstr}`, cid: CIDstr };
-}
-
-export const uploadToIpfs = (metadata: any, is_contract: boolean, localIpfs: boolean, verbose: boolean = false): Promise<ResultType> => {
-    const handler: handlerFunction = localIpfs ? uploadToLocal : uploadToNFTStorage;
-    return handler(metadata, is_contract, verbose);
 }
