264c2361932caca2cdbecbb7aa4f2d10a7b50954
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-12-01 08:41:12 +0100

    Moderation_v2 with MetaSettings + tests

--- contracts/mixins/Moderation_v2.py
+++ contracts/mixins/Moderation_v2.py
@@ -1,25 +1,28 @@
 import smartpy as sp
 
+utils = sp.io.import_script_from_url("file:contracts/utils/Utils.py")
+
 
 # Mixins required: Administrable
 class Moderation:
     """Mixin to add moderation_contract address to a contract's storage.
     
     Can then later be used through add moderation functions through upgrades."""
-    def __init__(self, meta_settings = False):
+    def __init__(self):
         self.update_initial_storage(
             moderation_contract = sp.set_type_expr(sp.none, sp.TOption(sp.TAddress))
         )
 
-        if meta_settings:
+        if hasattr(self, 'available_settings'):
             self.available_settings.append(
-                ("moderation_contract", sp.TOption(sp.TAddress), None)
+                ("moderation_contract", sp.TOption(sp.TAddress), lambda x : utils.ifSomeRun(x, lambda y: utils.isContract(y)))
             )
-            setattr(self, "set_moderation_contract", sp.entry_point(None, None))
+        else:
+            def set_moderation_contract(self, moderation_contract):
+                """Set moderation contract."""
+                sp.set_type(moderation_contract, sp.TOption(sp.TAddress))
+                self.onlyAdministrator()
+                utils.ifSomeRun(moderation_contract, lambda y: utils.isContract(y))
+                self.data.moderation_contract = moderation_contract
 
-    @sp.entry_point
-    def set_moderation_contract(self, moderation_contract):
-        """Set moderation contract."""
-        sp.set_type(moderation_contract, sp.TOption(sp.TAddress))
-        self.onlyAdministrator()
-        self.data.moderation_contract = moderation_contract
+            self.set_moderation_contract = sp.entry_point(set_moderation_contract)
--- tests/mixins/Moderation_v2_tests.py
+++ tests/mixins/Moderation_v2_tests.py
@@ -1,6 +1,7 @@
 import smartpy as sp
 
 Administrable = sp.io.import_script_from_url("file:contracts/mixins/Administrable.py").Administrable
+MetaSettings = sp.io.import_script_from_url("file:contracts/mixins/MetaSettings.py").MetaSettings
 mod_mixin = sp.io.import_script_from_url("file:contracts/mixins/Moderation_v2.py")
 
 
@@ -13,7 +14,20 @@ class ModerationTest(
         mod_mixin.Moderation.__init__(self)
 
 
-@sp.add_test(name = "Moderation_tests", profile = True)
+class ModerationTestMetaSettings(
+    Administrable,
+    mod_mixin.Moderation,
+    MetaSettings,
+    sp.Contract):
+    def __init__(self, administrator):
+        self.available_settings = []
+
+        Administrable.__init__(self, administrator = administrator)
+        mod_mixin.Moderation.__init__(self)
+        MetaSettings.__init__(self)
+
+
+@sp.add_test(name = "Moderation_v2_tests", profile = True)
 def test():
     admin = sp.test_account("Administrator")
     alice = sp.test_account("Alice")
@@ -35,9 +49,37 @@ def test():
 
     scenario.h3("set_moderation_contract")
 
-    moderation.set_moderation_contract(sp.some(bob.address)).run(sender = bob, valid = False)
+    # Check default
     scenario.verify(moderation.data.moderation_contract == sp.none)
-    moderation.set_moderation_contract(sp.some(bob.address)).run(sender = admin)
-    scenario.verify(moderation.data.moderation_contract == sp.some(bob.address))
+
+    # Check failure cases.
+    for t in [(bob, moderation.address, "ONLY_ADMIN"), (admin, bob.address, "NOT_CONTRACT")]:
+        sender, set_address, exception = t
+        moderation.set_moderation_contract(sp.some(set_address)).run(sender = sender, valid = False, exception = exception)
+
+    moderation.set_moderation_contract(sp.some(moderation.address)).run(sender = admin)
+    scenario.verify(moderation.data.moderation_contract == sp.some(moderation.address))
     moderation.set_moderation_contract(sp.none).run(sender = admin)
     scenario.verify(moderation.data.moderation_contract == sp.none)
+
+    # test meta settings
+    scenario.h2("Test Moderation - MetaSettings")
+
+    scenario.h3("Contract origination")
+    moderation_meta = ModerationTestMetaSettings(admin.address)
+    scenario += moderation_meta
+
+    scenario.h3("update_settings")
+
+    # Check default
+    scenario.verify(moderation_meta.data.moderation_contract == sp.none)
+
+    # Check failure cases.
+    for t in [(bob, moderation_meta.address, "ONLY_ADMIN"), (admin, bob.address, "NOT_CONTRACT")]:
+        sender, set_address, exception = t
+        moderation_meta.update_settings([sp.variant("moderation_contract", sp.some(set_address))]).run(sender = sender, valid = False, exception = exception)
+
+    moderation_meta.update_settings([sp.variant("moderation_contract", sp.some(moderation_meta.address))]).run(sender = admin)
+    scenario.verify(moderation_meta.data.moderation_contract == sp.some(moderation_meta.address))
+    moderation_meta.update_settings([sp.variant("moderation_contract", sp.none)]).run(sender = admin)
+    scenario.verify(moderation_meta.data.moderation_contract == sp.none)
