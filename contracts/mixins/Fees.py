d58a0c8cbebb3656730b71ff19d77f91f78ee2bc
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-12-01 08:59:58 +0100

    Fees with MetaSettings + tests

--- contracts/mixins/Fees.py
+++ contracts/mixins/Fees.py
@@ -3,35 +3,34 @@ import smartpy as sp
 
 # Required mixins: Administrable
 class Fees:
-    def __init__(self, fees_to, meta_settings = False):
+    def __init__(self, fees_to):
         self.update_initial_storage(
             fees = sp.nat(25),
             fees_to = fees_to
         )
 
-        if meta_settings:
+        if hasattr(self, 'available_settings'):
             self.available_settings.extend([
                 ("fees", sp.TNat, lambda x: sp.verify(x <= 60, message = "FEE_ERROR")),
                 ("fees_to", sp.TAddress, None)
             ])
-            setattr(self, "update_fees", sp.entry_point(None, None))
-            setattr(self, "update_fees_to", sp.entry_point(None, None))
+        else:
+            def update_fees(self, fees):
+                """Call to set fees in permille or fee recipient.
+                Fees must be <= than 60 permille.
+                """
+                sp.set_type(fees, sp.TNat)
+                self.onlyAdministrator()
+                sp.verify(fees <= 60, message = "FEE_ERROR") # let's not get greedy
+                self.data.fees = fees
 
-    @sp.entry_point
-    def update_fees(self, fees):
-        """Call to set fees in permille or fee recipient.
-        Fees must be <= than 60 permille.
-        """
-        sp.set_type(fees, sp.TNat)
-        self.onlyAdministrator()
-        sp.verify(fees <= 60, message = "FEE_ERROR") # let's not get greedy
-        self.data.fees = fees
+            def update_fees_to(self, fees_to):
+                """Set fee recipient.
+                """
+                sp.set_type(fees_to, sp.TAddress)
+                self.onlyAdministrator()
+                self.data.fees_to = fees_to
 
-    @sp.entry_point
-    def update_fees_to(self, fees_to):
-        """Set fee recipient.
-        """
-        sp.set_type(fees_to, sp.TAddress)
-        self.onlyAdministrator()
-        self.data.fees_to = fees_to
+            self.update_fees = sp.entry_point(update_fees)
+            self.update_fees_to = sp.entry_point(update_fees_to)
 
--- tests/mixins/Fees_tests.py
+++ tests/mixins/Fees_tests.py
@@ -1,6 +1,7 @@
 import smartpy as sp
 
 Administrable = sp.io.import_script_from_url("file:contracts/mixins/Administrable.py").Administrable
+MetaSettings = sp.io.import_script_from_url("file:contracts/mixins/MetaSettings.py").MetaSettings
 fees_mixin = sp.io.import_script_from_url("file:contracts/mixins/Fees.py")
 
 
@@ -11,6 +12,19 @@ class FeesTest(
     def __init__(self, administrator, fees_to):
         Administrable.__init__(self, administrator = administrator)
         fees_mixin.Fees.__init__(self, fees_to = fees_to)
+
+
+class FeesTestMetaSettings(
+    Administrable,
+    fees_mixin.Fees,
+    MetaSettings,
+    sp.Contract):
+    def __init__(self, administrator, fees_to):
+        self.available_settings = []
+
+        Administrable.__init__(self, administrator = administrator)
+        fees_mixin.Fees.__init__(self, fees_to = fees_to)
+        MetaSettings.__init__(self)
 
 
 @sp.add_test(name = "Fees_tests", profile = True)
@@ -33,20 +47,63 @@ def test():
     fees = FeesTest(admin.address, admin.address)
     scenario += fees
 
+    # Check default
+    scenario.verify(fees.data.fees == sp.nat(25))
+    scenario.verify(fees.data.fees_to == admin.address)
+
     #
     # update_fees
     #
     scenario.h3("update_fees")
 
-    fees.update_fees(35).run(sender = bob, valid = False)
-    fees.update_fees(250).run(sender = admin, valid = False)
-    scenario.verify(fees.data.fees == sp.nat(25))
+    # Test failiure cases
+    for t in [(bob, sp.nat(35), "ONLY_ADMIN"), (admin, sp.nat(250), "FEE_ERROR")]:
+        sender, fee_amount, exception = t
+        fees.update_fees(fee_amount).run(sender = sender, valid = False, exception = exception)
+
     fees.update_fees(45).run(sender = admin)
     scenario.verify(fees.data.fees == sp.nat(45))
 
     scenario.h3("update_fees_to")
 
-    fees.update_fees_to(bob.address).run(sender = bob, valid = False)
-    scenario.verify(fees.data.fees_to == admin.address)
-    fees.update_fees_to(bob.address).run(sender = admin)
-    scenario.verify(fees.data.fees_to == bob.address)
+    # No permission for anyone but admin
+    for acc in [alice, bob, admin]:
+        fees.update_fees_to(bob.address).run(
+            sender = acc,
+            valid = (True if acc is admin else False),
+            exception = (None if acc is admin else "ONLY_ADMIN"))
+
+        if acc is admin: scenario.verify(fees.data.fees_to == bob.address)
+
+    # Test meta settings
+    scenario.h2("Test Fees - MetaSettings")
+
+    scenario.h3("Contract origination")
+    fees_meta = FeesTestMetaSettings(admin.address, admin.address)
+    scenario += fees_meta
+
+    # Check default
+    scenario.verify(fees_meta.data.fees == sp.nat(25))
+    scenario.verify(fees_meta.data.fees_to == admin.address)
+
+    #
+    # update_settings
+    #
+    scenario.h3("update_settings")
+
+    # Test failiure cases
+    for t in [(bob, sp.nat(35), "ONLY_ADMIN"), (admin, sp.nat(250), "FEE_ERROR")]:
+        sender, fee_amount, exception = t
+        fees_meta.update_settings([sp.variant("fees", fee_amount)]).run(sender = sender, valid = False, exception = exception)
+
+    fees_meta.update_settings([sp.variant("fees", sp.nat(45))]).run(sender = admin)
+    scenario.verify(fees_meta.data.fees == sp.nat(45))
+
+    # No permission for anyone but admin
+    for acc in [alice, bob, admin]:
+        fees_meta.update_settings([sp.variant("fees_to", bob.address)]).run(
+            sender = acc,
+            valid = (True if acc is admin else False),
+            exception = (None if acc is admin else "ONLY_ADMIN"))
+
+        if acc is admin: scenario.verify(fees_meta.data.fees_to == bob.address)
