4bf0e0bd648c7e14b9bce314d1d10eeae0cee638
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-12-05 23:34:39 +0100

    Utils: spacing

--- contracts/utils/Utils.py
+++ contracts/utils/Utils.py
@@ -6,14 +6,20 @@ import smartpy as sp
 # Utility functions
 #
 
+#CONTRACT_ADDRESS_LOW = sp.address("KT18amZmM5W7qDWVt2pH6uj7sCEd3kbzLrHT")
+#CONTRACT_ADDRESS_HIGH = sp.address("KT1XvNYseNDJJ6Kw27qhSEDF8ys8JhDopzfG")
+
+
 def isPowerOfTwoMinusOne(x):
     """Returns expression that evaluates to true if x is power of 2 - 1"""
     return ((x + 1) & x) == sp.nat(0)
+
 
 def sendIfValue(to, amount):
     """Transfer `amount` to `to` if greater 0."""
     with sp.if_(amount > sp.tez(0)):
         sp.send(to, amount)
+
 
 @sp.inline_result
 def openSomeOrDefault(e: sp.TOption, default):
@@ -24,12 +30,11 @@ def openSomeOrDefault(e: sp.TOption, default):
         with arg.match("Some") as open:
             sp.result(open)
 
+
 def ifSomeRun(e: sp.TOption, f):
     """If option `e` is some, execute function `f`."""
     with e.match("Some") as open: f(open)
 
-#CONTRACT_ADDRESS_LOW = sp.address("KT18amZmM5W7qDWVt2pH6uj7sCEd3kbzLrHT")
-#CONTRACT_ADDRESS_HIGH = sp.address("KT1XvNYseNDJJ6Kw27qhSEDF8ys8JhDopzfG")
 
 def isContract(addr: sp.TAddress):
     """Returns expression that checks if `addr` is a contract address.
@@ -39,9 +44,11 @@ def isContract(addr: sp.TAddress):
     #return (addr >= CONTRACT_ADDRESS_LOW) & (addr <= CONTRACT_ADDRESS_HIGH) # prob best not to.
     return sp.slice(sp.pack(sp.set_type_expr(addr, sp.TAddress)), 6, 1) == sp.some(sp.bytes("0x01"))
 
+
 def onlyContract(addr: sp.TAddress):
     """Fails with NOT_CONTRACT if `addr` is not a contract address."""
     sp.verify(isContract(addr), "NOT_CONTRACT")
+
 
 def validateIpfsUri(metadata_uri: sp.TBytes):
     """Validate IPFS Uri.
@@ -50,6 +57,7 @@ def validateIpfsUri(metadata_uri: sp.TBytes):
     Ipfs cid v0 + proto is 53 chars."""
     sp.verify((sp.slice(metadata_uri, 0, 7) == sp.some(sp.utils.bytes_of_string("ipfs://")))
         & (sp.len(metadata_uri) >= sp.nat(53)), "INVALID_METADATA")
+
 
 def contractSetMetadata(contract, metadata_uri):
     """ContractMetadata set_metadata."""
